apsavin/try-bem-online__front

Step 2. Hello world

app/example/example.bemdecl.js
@@ -1,0 +1,10 @@
+exports.blocks = [
+
+    // bem-node part
+    {block: 'i-console'}, //colorful console log
+    {block: 'i-ycssjs'}, //output static files
+    {block: 'i-bem-node'}, //bem-node api
+
+    // pages
+    {block: 'hello-world'}
+];

bem-node/bem-node-hello-world

app/example/example.bemdecl.js
@@ -1,0 +1,10 @@
+exports.blocks = [
+
+    // bem-node part
+    {block: 'i-console'}, //colorful console log
+    {block: 'i-enb'}, //output static files
+    {block: 'i-bem-node'}, //bem-node api
+
+    // pages
+    {block: 'hello-world'}
+];